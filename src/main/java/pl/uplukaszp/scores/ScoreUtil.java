94cd7cae98e567f4b5484676fded4146ee041d59
Łukasz <uplukaszp1@gmail.com>
2018-11-29 17:36:35 +0100

    Update ScoreUtil comments

--- src/main/java/pl/uplukaszp/scores/ScoreUtil.java
+++ src/main/java/pl/uplukaszp/scores/ScoreUtil.java
@@ -12,14 +12,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ScoreUtil {
+	private static final String FILE_NAME = "save";
+
+	/**
+	 * Loads scores from file, appends new score, sort all of them and save 10 best
+	 * scores in file
+	 */
 	public static void save(String name, String points) {
 		ObjectOutputStream stream = null;
 		List<Score> scores = load();
 		scores.add(new Score(name, points));
 		List<Score> topTen = scores.stream().sorted().limit(10).collect(Collectors.toList());
-		Serializable toStream=(Serializable) topTen;
+		Serializable toStream = (Serializable) topTen;
 		try {
-			stream = new ObjectOutputStream(new FileOutputStream(new File("save")));
+			stream = new ObjectOutputStream(new FileOutputStream(new File(FILE_NAME)));
 			stream.writeObject(toStream);
 			stream.flush();
 		} catch (IOException e) {
@@ -33,14 +39,18 @@ public class ScoreUtil {
 		}
 	}
 
+	/**
+	 * Loads scores from file, saved best scores. When file not exist, is broken etc
+	 * return empty list
+	 */
 	@SuppressWarnings("unchecked")
 	public static List<Score> load() {
 		ObjectInputStream stream = null;
 		List<Score> scores = null;
 		try {
-			stream = new ObjectInputStream(new FileInputStream(new File("save")));
+			stream = new ObjectInputStream(new FileInputStream(new File(FILE_NAME)));
 			Object temp = stream.readObject();
-				scores = (List<Score>) temp;
+			scores = (List<Score>) temp;
 		} catch (Exception e) {
 		} finally {
 			if (stream != null) {
